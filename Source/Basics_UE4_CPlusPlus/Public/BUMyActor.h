9b2a55b3a3a5d60f4745d2808b4dec7bd37cc993
Yaphet <find.mr.y@gmail.com>
2019-09-04 10:26:14 -0500

    Classes Overview - Initial

---
+++ Source/Basics_UE4_CPlusPlus/Public/BUMyActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Standard Include. Unreal bits and pieces
+#include "CoreMinimal.h"
+
+// Parent class
+#include "GameFramework/Actor.h"
+
+// Allows the use of the Unreal Header Tool (Interface between editor and code)
+#include "BUMyActor.generated.h"
+
+// UCLASS Macro is telling the Unreal Header Tool that this is a class we need to know about in the Editor (Blueprint refs, world outliner, etc)
+// Class Specifiers that can be put into the parenthesis
+// https://docs.unrealengine.com/en-US/Programming/UnrealArchitecture/Reference/Classes/Specifiers/index.html
+UCLASS()
+class BASICS_UE4_CPLUSPLUS_API ABUMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABUMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
