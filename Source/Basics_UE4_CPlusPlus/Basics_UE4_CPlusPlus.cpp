08147068a961963d62488204c74b3a72c27e6c62
YaphBlaster <find.mr.y@gmail.com>
2019-09-10 10:27:48 -0500

    Game Module comments

--- Source/Basics_UE4_CPlusPlus/Basics_UE4_CPlusPlus.cpp
+++ Source/Basics_UE4_CPlusPlus/Basics_UE4_CPlusPlus.cpp
@@ -3,4 +3,5 @@
 #include "Basics_UE4_CPlusPlus.h"
 #include "Modules/ModuleManager.h"
 
+// This tells Unreal Engine "hey this Project is just another module, add it to the engine"
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Basics_UE4_CPlusPlus, "Basics_UE4_CPlusPlus" );
